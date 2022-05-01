24ad2cfdd272253aaecef542687acb83fa1f1d02
SarkerPabon <sarkerpabon@gmail.com>
2022-05-01 11:44:18 +0600

    Heroku Deploy

---
+++ index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+function verifyJWT(req, res, next) {
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader) {
+		return res.status(401).send({ message: "Unauthorized Access" });
+	}
+
+	const token = authHeader.split(" ")[1];
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			return res.status(403).send({ message: "Frobidden access" });
+		}
+		console.log("Decoded: ", decoded);
+		req.decoded = decoded;
+	});
+
+	// console.log("Inside VerifyJWT: ", authHeader);
+	next();
+}
+
+// Database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.riqon.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+/* client.connect((err) => {
+	const collection = client.db("test").collection("devices");
+	console.log("Genius Car Service DB");
+
+	client.close();
+}); */
+
+async function run() {
+	await client.connect();
+	const serviceCollection = client.db("geniusCar").collection("service");
+	const orderCollection = client.db("geniusCar").collection("order");
+
+	// Auth
+	app.post("/login", async (req, res) => {
+		const user = req.body;
+		const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+			expiresIn: "1d",
+		});
+
+		res.send({ accessToken });
+	});
+
+	// Servicess API
+	app.get("/service", async (req, res) => {
+		const query = {};
+		const cursor = serviceCollection.find(query);
+		const services = await cursor.toArray();
+
+		res.send(services);
+	});
+
+	// POST
+	app.post("/service", async (req, res) => {
+		const newService = req.body;
+		const result = await serviceCollection.insertOne(newService);
+
+		res.send(result);
+	});
+
+	// DELETE
+	app.delete("/service/:id", async (req, res) => {
+		const id = req.params.id;
+		const query = { _id: ObjectId(id) };
+		const result = await serviceCollection.deleteOne(query);
+		res.send(result);
+	});
+
+	app.get("/service/:id", async (req, res) => {
+		const id = req.params.id;
+		const query = { _id: ObjectId(id) };
+
+		const service = await serviceCollection.findOne(query);
+
+		res.send(service);
+	});
+
+	// Order Colection API
+	app.get("/order", verifyJWT, async (req, res) => {
+		// const authHeader = req.headers.authorization;
+		// console.log(authHeader);
+
+		const decodedEmail = req.decoded.email;
+
+		const email = req.query.email;
+		console.log(email);
+		const query = { email };
+
+		if (email === decodedEmail) {
+			const cursor = orderCollection.find(query);
+			const orders = await cursor.toArray();
+			res.send(orders);
+		} else {
+			res.status(403).send({ message: "Forbidden Access" });
+		}
+	});
+
+	app.post("/order", async (req, res) => {
+		const order = req.body;
+		const result = await orderCollection.insertOne(order);
+		res.send(result);
+	});
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("Running Genius Server");
+});
